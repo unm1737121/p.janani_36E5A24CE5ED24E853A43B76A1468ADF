0e0d47cec26a0b120bac4a6da9240778b7f19ee4
unm1737121 <147687941+unm1737121@users.noreply.github.com>
2023-10-12 14:26:32 +0530

    Add files via upload

--- challenge unit 2/main (2).py
+++ challenge unit 2/main (2).py
@@ -1,22 +1,23 @@
+#cDefine the derived class Batman 
 class player:
-
-  def play (player):
-    print("the player is playing cricket.")
-
-
-class batsman(player):
-
-  def player(self):
-    print(" the batsman is batting.")
+  def play(self):
+    print("The player is")
 
 
-class bowler(player):
 
-  def play(player):
-    print("the bowler is bowling.")
+class Batsman(player):
+  def play(self):
+    print("The Batsman is batting.")
 
+# Define the derived class Bowler 
+class Bowler(player):
+  def play(self):
+    print("The Bowler is bowling.")
 
-batsman = batsman()
-bowler = bowler()
+# Create objects of Batsman and Bowler classesclasses
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play()Method for each object 
 batsman.play()
 bowler.play()
